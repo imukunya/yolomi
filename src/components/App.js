959019b343773473b7a07573eab30a50ea43d0fb
Ian Mukunya <imukunya@safaricom.co.ke>
2020-08-13 08:47:41 +0300

    basic site structure

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/App.css';
+import Header from './Header';
+import AboutUs from './AboutUs';
+import Footer from './Footer';
+import ProductList from './ProductList';  
+
+function App() {
+ return (
+   <React.Fragment>
+     <div className="container-fluid">
+       <Header />
+       <ProductList />
+       <AboutUs />
+       <Footer />
+     </div>
+   </React.Fragment>   
+ )
+}
+
+export default App;
