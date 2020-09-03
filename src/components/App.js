8db321a23bf45fade5590772ad0c28ae6f11b314
Ian Mukunya <imukunya@safaricom.co.ke>
2020-09-03 08:43:15 +0300

    code commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import '../styles/App.css';
 import Header from './Header';
 import AboutUs from './AboutUs';
 import Footer from './Footer';
-import ProductList from './ProductList';  
+import ProductControl from './ProductControl';
 
 function App() {
  return (
    <React.Fragment>
      <div className="container-fluid">
        <Header />
-       <ProductList />
+       <ProductControl />
        <AboutUs />
        <Footer />
      </div>
